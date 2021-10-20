321e948715c9d6df080264b8f5cfc49a66fdd16a
unknown <ivgorlik@gmail.com>
2021-10-20 18:09:48 +0300

    split to models

--- ds_management/models.py
+++ ds_management/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from django.db.models import Max, Min
 from django.contrib.postgres.indexes import HashIndex
-from django.contrib.auth.models import  BaseUserManager
-from copy import deepcopy
+from django.contrib.auth.models import BaseUserManager
+from ds_management.string_constraints.string_constraints import *
 from typing import Dict
 
 import json
-from ds_management.string_constraints.string_constraints import *
 
 
 class ItemCategory(models.Model):
@@ -15,78 +13,33 @@ class ItemCategory(models.Model):
     def __str__(self) -> models.CharField:
         return self.category_name
 
-    def __unicode__(self)-> models.CharField:
+    def __unicode__(self) -> models.CharField:
         return self.category_name
 
 
 class Item(models.Model):
     item_name: models.CharField = models.CharField(max_length=250, unique=True)
     category_name: models.ForeignKey = models.ForeignKey(ItemCategory,
-                                      choices=STRUCTURE_CHOICES,
-                                      on_delete=models.CASCADE,
-                                      default=1
-                                      )
+                                                         choices=STRUCTURE_CHOICES,
+                                                         on_delete=models.CASCADE,
+                                                         default=1
+                                                         )
     owner: models.ForeignKey = models.ForeignKey(
-                                                'auth.User',
-                                                related_name='items',
-                                                on_delete=models.CASCADE,
-                                                default=0
+        'auth.User',
+        related_name='items',
+        on_delete=models.CASCADE,
+        default=0
 
-                                            )
+    )
 
     class Meta:
         unique_together = ('owner', 'item_name')
 
-    def __str__(self)->models.CharField:
+    def __str__(self) -> models.CharField:
         return self.item_name
 
 
-class ItemElementManager(BaseUserManager):
-    def pop(self, item_id) -> Dict:
-        """
-        remove last element from DS
-
-        :param item_id:
-        :return:
-        """
-
-        response_result: Dict = None
-        structure_type: str = Item.objects.get(pk=item_id).category_name.category_name
-        if structure_type and structure_type is DataStructures.queue or DataStructures.stack:
-            response_result: Dict = StackQueue.objects.pop(item_id)
-        return response_result
-
-    def peek(self, item_id) -> Dict:
-        """
-       see last item in DS
-
-        :param item_id:
-        :return:
-        """
-
-        response_result: Dict = None
-        structure_type: str = Item.objects.get(pk=item_id).category_name.category_name
-        if structure_type and structure_type is DataStructures.queue or DataStructures.stack:
-            response_result: Dict = StackQueue.objects.pop(item_id,remove_item=False)
-        return response_result
-
-    def add_new_element(self, item_id, element_data)->Dict:
-        structure_type: str = Item.objects.get(pk=item_id).category_name.category_name
-        element: ItemElement = ItemElement.objects.create(item_id=item_id,
-                                             element_data=element_data,
-                                             )
-
-        if structure_type and structure_type is \
-                DataStructures.queue or DataStructures.stack or DataStructures.list:
-            new_element: Dict = StackQueue.objects.push(item_id=item_id,
-                                                        element_id=element.id,
-                                                        )
-
-            return new_element
-
-
 class ItemElement(models.Model):
-    objects = ItemElementManager()
     item = models.ForeignKey(
         Item,
         related_name='item_elements',
@@ -99,65 +52,6 @@ class ItemElement(models.Model):
     def __str__(self):
         return json.dumps(self.element_data)
 
-# todo to implement as linked list:https://isaaccomputerscience.org/concepts/dsa_datastruct_stack?examBoard=all&stage=all
-class StackQueueManager(models.Manager):
-
-    def push(self, item_id, element_id):
-        """
-        push new element for item:
-        create new one
-        :param item_id:
-        :param element_id:
-        :return:
-        """
-        if self.is_empty(item_id):
-            new_element: ItemElement = self.create(item_id=item_id,
-                                      element_id=element_id,
-                                      position=0
-                                      )
-
-        else:
-            element_to_push_index: int = self.filter(item_id=item_id).aggregate(Max(position_str))['position__max']
-            new_index: int = element_to_push_index + 1
-            new_element: ItemElement = self.create(item_id=item_id,
-                                      element_id=element_id,
-                                      position=new_index
-                                      )
-
-        return new_element
-
-    # todo to implement as linked list:https://isaaccomputerscience.org/concepts/dsa_datastruct_stack?examBoard=all&stage=all
-    def pop(self, item_id, remove_item = True) -> ItemElement:
-        item: Item = Item.objects.get(pk=item_id)
-        structure_type: str = item.category_name.category_name
-        stack_queue_element_to_pop_position = None
-        if not self.is_empty(item_id):
-            if structure_type == DataStructures.stack:
-                stack_queue_element_to_pop_position: int = self.filter(item_id=item_id).aggregate(Max(position_str))['position__max']
-            elif structure_type in [DataStructures.queue, DataStructures.list]:
-                stack_queue_element_to_pop_position: int = self.filter(item_id=item_id).aggregate(Min(position_str))['position__min']
-            stack_queue_element_to_pop: StackQueue = self.get(position=stack_queue_element_to_pop_position, item_id=item_id)
-            element_to_pop: ItemElement = ItemElement.objects.get(id=stack_queue_element_to_pop.element.id)
-            new_instance: ItemElement = deepcopy(element_to_pop)
-            if remove_item:
-                element_to_pop.delete()
-                stack_queue_element_to_pop.delete()
-
-            return new_instance
-
-
-
-
-    # todo to implement as linked list:https://isaaccomputerscience.org/concepts/dsa_datastruct_stack?examBoard=all&stage=all
-    def is_empty(self, item_id) ->bool:
-        """
-        check if data structure have any element left
-        :param item_id:
-        :return: item element
-        """
-        have_items = self.filter(item_id=item_id).exists()
-        return not have_items
-
 
 class StackQueue(models.Model):
     position = models.IntegerField()
@@ -169,7 +63,6 @@ class StackQueue(models.Model):
         ItemElement,
         related_name='+',
         on_delete=models.CASCADE)
-    objects = StackQueueManager()
 
     class Meta:
         unique_together = ('item_id', 'position',)
