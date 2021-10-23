97fbbea0953c723f92d36b4e8308c2c5c0813686
unknown <ivgorlik@gmail.com>
2021-10-23 08:11:55 +0300

    ds modules

---
+++ ds_management/ds_models/item_element_ds.py
@@ -0,0 +1,42 @@
+from ds_management.models import ItemElement,Item,StackQueue
+
+from django.db import models
+
+from ds_management.string_constraints.string_constraints import *
+
+#
+# class ItemElemenManager(models.Manager):
+#     @staticmethod
+#     def pop(item_id) -> Dict:
+#         """
+#         remove last element from DS
+#
+#         :param item_id:
+#         :return:
+#         """
+#
+#         response_result: Dict = StackQueueDs.pop(item_id)
+#         return response_result
+#
+#     @staticmethod
+#     def peek(item_id) -> Dict:
+#         """
+#        see last item in DS
+#
+#         :param item_id:
+#         :return:
+#         """
+#
+#         response_result: Dict = StackQueueDs.pop(item_id, remove_item=False)
+#         return response_result
+#
+#     @staticmethod
+#     def push(item_id, element_data)->Dict:
+#         element: ItemElement = ItemElement.objects.create(item_id=item_id,
+#                                              element_data=element_data,
+#                                              )
+#         new_element: Dict = StackQueueDs.push(item_id=item_id,
+#                                               element_id=element.id,
+#                                               )
+#
+#         return new_element
